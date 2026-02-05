670c72ec895a0c226417a8bdfc472f96cba44375
Alexander Johnston <alexanderj.roaming@gmail.com>
2026-02-05 13:16:47 -0500

    Update MVC and SignalR for net10

--- src/Totem.Timeline.SignalR/Hosting/TimelineHubRouteBuilderExtensions.cs
+++ src/Totem.Timeline.SignalR/Hosting/TimelineHubRouteBuilderExtensions.cs
@@ -1,20 +1,21 @@
 using System;
 using System.ComponentModel;
 using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.SignalR;
 
 namespace Totem.Timeline.SignalR.Hosting
 {
   /// <summary>
-  /// Extends <see cref="HubRouteBuilder"/> to declare timeline hubs
+  /// Extends <see cref="IEndpointRouteBuilder"/> to declare timeline hubs
   /// </summary>
   [EditorBrowsable(EditorBrowsableState.Never)]
   public static class TimelineHubRouteBuilderExtensions
   {
-    public static void MapQueryHub(this HubRouteBuilder routes, Action<HttpConnectionDispatcherOptions> configure, string path = "/hubs/query") =>
+    public static void MapQueryHub(this IEndpointRouteBuilder routes, Action<HttpConnectionDispatcherOptions> configure, string path = "/hubs/query") =>
       routes.MapHub<QueryHub>(path, configure);
 
-    public static void MapQueryHub(this HubRouteBuilder routes, string path = "/hubs/query") =>
+    public static void MapQueryHub(this IEndpointRouteBuilder routes, string path = "/hubs/query") =>
       routes.MapHub<QueryHub>(path);
   }
 }
