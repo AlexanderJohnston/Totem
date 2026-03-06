c45bdcc7e4b3e8ae66efe71b6e12de5af94b64b6
Alexander Johnston <alexanderj.roaming@gmail.com>
2026-03-06 08:11:23 -0500

    Add a samy filter.

--- Outermind/Topics/SmartScanner.cs
+++ Outermind/Topics/SmartScanner.cs
@@ -22,7 +22,7 @@ namespace Outermind.Topics
       {
         var path = scan.FolderPath ?? string.Empty;
         var pathLower = path.ToLowerInvariant();
-        var isRejectedPath = pathLower.Contains("3-indexing") || pathLower.Contains("1-ip") || pathLower.Contains("3-samy");
+        var isRejectedPath = pathLower.Contains("3-indexing") || pathLower.Contains("1-ip") || pathLower.Contains("3-samy") || pathLower.Contains("samy");
 
         if(isRejectedPath)
         {
